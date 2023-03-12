efbedcfdac3938dd81fcb42e61c9f12b8a04b419
lab <ywonchall@163.com>
2023-03-12 19:08:50 +0800

    update config

--- config.sh
+++ config.sh
@@ -1,4 +1,8 @@
 pip install torch==1.9.0+cu111 torchvision==0.10.0+cu111 torchaudio==0.9.0 -f https://download.pytorch.org/whl/torch_stable.html
+cd OpenLane-V2
+pip install -r requirements.txt
+python setup.py develop
+cd ..
 pip install mmcv-full==1.5.2
 pip install mmdet==2.26.0
 pip install mmsegmentation==0.29.1
